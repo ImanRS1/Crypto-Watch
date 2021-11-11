17a9f1a2430d0354c41415ee0607ee2f98e26fbf
Iman Reza Soltani <iman.rezasoltani@gmail.com>
2021-11-11 10:09:27 +0100

    backend server setup, init api call implemented

---
+++ server/App.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const port = 5000;
+
+app.get("/", async (req, res) => {
+  try {
+    await axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+      )
+      .then((data) =>
+        res
+          .header("Access-Control-Allow-Origin", "*")
+          .status(200)
+          .send(data.data)
+      )
+      .catch((err) => res.send(err));
+  } catch (err) {
+    console.error("GG", err);
+  }
+});
+
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
