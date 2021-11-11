41eaa6c24efa88434b1174a140befcdee75c236b
Iman Reza Soltani <iman.rezasoltani@gmail.com>
2021-11-11 10:15:31 +0100

    api call refactored

--- server/App.js
+++ server/App.js
@@ -3,23 +3,26 @@ const axios = require("axios");
 const app = express();
 const port = 5000;
 
-app.get("/", async (req, res) => {
-  try {
-    await axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=10&page=1&sparkline=false"
-      )
-      .then((data) =>
-        res
-          .header("Access-Control-Allow-Origin", "*")
-          .status(200)
-          .send(data.data)
-      )
-      .catch((err) => res.send(err));
-  } catch (err) {
-    console.error("GG", err);
-  }
-});
+
+const getAllCoins = async (req, res) => {
+    try {
+        await axios
+          .get(
+            "https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+          )
+          .then((data) =>
+            res
+              .header("Access-Control-Allow-Origin", "*")
+              .status(200)
+              .send(data.data)
+          )
+          .catch((err) => res.send(err));
+      } catch (err) {
+        console.error("GG", err);
+      }
+  };
+
+app.get('/', getAllCoins);
 
 
 app.listen(port, () => {
