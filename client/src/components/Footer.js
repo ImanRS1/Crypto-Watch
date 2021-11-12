8a93c030da37b5e5e87d72a86ca7fceba1c80c5a
Iman Reza Soltani <iman.rezasoltani@gmail.com>
2021-11-12 09:18:08 +0100

    footer styled

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,10 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import iLogo from '../images/I.png'
+import geckoLogo from '../images/gecko.png'
 
 const Footer = () => {
   return (
     <FooterContainer>
-      <FooterTitle>Github Logo</FooterTitle>
+      <FooterTitle>
+        <div className="dev-text"><a href="https://github.com/ImanRS1/Crypto-Watch">Developed by <img src={iLogo} alt={iLogo} /></a></div>
+        <div className="api-text"><a href="https://www.coingecko.com/en/api/documentation">Powered by <img src={geckoLogo} alt={geckoLogo} /></a></div>
+        
+        </FooterTitle>
     </FooterContainer>
   );
 };
@@ -16,11 +22,40 @@ const FooterContainer = styled.div`
   width: 100vw;
   bottom: 0;
   position: sticky;
-  height: 5rem;
+  height: 4.2rem;
+  border-top: 1px solid black;
 `;
 
 const FooterTitle = styled.div`
-  padding: 1rem;
+  box-sizing: border-box;
+  padding: 0.4rem;
   font-size: 1.2rem;
   box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  .dev-text a{
+    display: flex;
+    align-items: center;
+  }
+
+  .api-text {
+    margin-top: 0.5rem;
+    font-size: 1rem;
+   
+  }
+
+  .api-text a{
+    display: flex;
+    align-items: center;
+  }
+
+  img{
+    margin-left: 0.2rem;
+    margin-top: 0.2rem;
+    width: 1rem;
+    height: 1rem;
+  }
 `;
