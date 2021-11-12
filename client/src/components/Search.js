b30375edfe29ab1c2abdfba0887aa5ce3477fe74
Iman Reza Soltani <iman.rezasoltani@gmail.com>
2021-11-12 11:44:24 +0100

    searchbar styled

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -13,7 +13,7 @@ const Search = ({ getSearchedCoin }) => {
   return (
     <FormContainer>
       <form onSubmit={handleSubmit}>
-        <input type="text" required />
+        <input type="text" required placeholder="Search a coin..." />
         <button type="submit">Search</button>
       </form>
     </FormContainer>
@@ -24,4 +24,29 @@ export default Search;
 
 const FormContainer = styled.div`
   margin: 2rem;
+
+  input {
+    padding: 0.5rem 0.5rem;
+    width: 15rem;
+    border: none;
+    border-top-left-radius: 0.8rem;
+    border-bottom-left-radius: 0.8rem;
+    &:focus {
+      outline: none;
+    }
+  }
+  button {
+    font: black;
+    padding: 0.5rem 0;
+    border: none;
+    width: 5rem;
+    border-top-right-radius: 0.8rem;
+    border-bottom-right-radius: 0.8rem;
+    &:hover {
+      cursor: pointer;
+    }
+    &:focus {
+      outline: none;
+    }
+  }
 `;
